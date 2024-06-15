a606e172448e8b86af557bda12bd3c0e7c8085e2
fphper <1076699625@qq.com>
2024-06-15 17:26:30 +0800

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'e6f8a6f3-2961-c521-5bac-36d86cf651f1';
 
-let proxyIP = '';
+let proxyIP = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
 
 
 if (!isValidUUID(userID)) {
